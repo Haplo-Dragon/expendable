169c9753d35983d8db6ddc7e1b1c72192d4c4357
Ethan Fulbright <haplodragon@gmail.com>
2020-05-27 02:55:22 -0500

    Avoid creating duplicate expendable bookmarks

--- src/background.ts
+++ src/background.ts
@@ -1,11 +1,17 @@
+// We'll keep track of the expendable bookmarks folder by its ID, allowing it
+// to be moved or renamed if the user wants.
 const FOLDER_NAME = "Expendable Bookmarks"
 let FOLDER_ID: string
+
 const NOTIFICATION_ICON = "icons/star-expendable-48.png"
 const NOTIFICATION_TIMEOUT_MILLISECONDS = 3000
+
 // We'll keep track of expendable bookmarks by their Firefox-assigned id.
 const expendable_bookmark_set = new Set<string>()
 
 
+// Find the expendable bookmarks folder (or create it if it's not present), and
+// register any already-existing expendable bookmarks inside it.
 init_expendable_bookmarks()
 
 function init_expendable_bookmarks() {
@@ -14,15 +20,23 @@ function init_expendable_bookmarks() {
             if (items.length > 0) {
                 console.log("Found expendable folder.")
             } else {
+                // If the expendable folder doesn't exist, we'll create it, then grab
+                // its ID.
                 console.log("Expendable bookmarks folder not found, creating...")
                 browser.bookmarks.create( {title: FOLDER_NAME} )
+                    .then (new_expendable_folder => {
+                        FOLDER_ID = new_expendable_folder.id
+                    })
             }            
+            // If the expendable folder does exist, we'll check it for bookmarks.
             return browser.bookmarks.getSubTree(items[0].id)
         })
+        // Record the expendable folder's ID and keep track of any bookmarks in it.
         .then ( items => {            
-            console.log("Found subtree, recording ID and adding bookmarks to set...")
+            console.log("Found subtree, recording ID and adding bookmarks to set...")            
             FOLDER_ID = items[0].id
             buildSet(items[0])
+            
             console.log("Expendable bookmark set initialized:")         
             console.log(expendable_bookmark_set)
         })
@@ -50,14 +64,18 @@ function buildSet(bookmark_item: browser.bookmarks.BookmarkTreeNode) {
 
 
 function createExpendableBookmark(bookmark_title: string, bookmark_url: string) {
-    let bookmark_details = getBookmarkDetails(bookmark_title, bookmark_url)
+    // Avoid duplicate expendable bookmarks.
+    alreadyExists(bookmark_url)
+        .then( bookmark_already_exists => {
+            // If it's a new bookmark (that is, NOT a duplicate), we'll create it.
+            if (!bookmark_already_exists) {
+                let bookmark_details = getBookmarkDetails(bookmark_title, bookmark_url)
 
-    browser.bookmarks.create(bookmark_details)
-        // .then( new_bookmark_node => {
-        //     // addExpendableBookmark(new_bookmark_node)
-        // })
-        .catch( error => {
-            console.log(`Couldn't create new expendable bookmark: ${error}`)
+                browser.bookmarks.create(bookmark_details)            
+                    .catch( error => {
+                        console.log(`Couldn't create new expendable bookmark: ${error}`)
+                    })
+                        }
         })
 }
 
@@ -71,27 +89,59 @@ function getBookmarkDetails(bookmark_title: string, bookmark_url: string) {
     return bookmark_details
 }
 
-function addExpendableBookmark(bookmark: browser.bookmarks.BookmarkTreeNode) {
-    expendable_bookmark_set.add(bookmark.id)
-    console.log(`Added new expendable bookmark ${bookmark.title}.`)
-    
-    browser.notifications.create( {
-        type: "basic",
-        title: "Expendable Bookmark Created!",
-        message: `New expendable bookmark created: ${bookmark.title}`,
-        iconUrl: NOTIFICATION_ICON
-    })
-        .then( notification_id => {
-            setTimeout(() => 
-                browser.notifications.clear(notification_id),
-                NOTIFICATION_TIMEOUT_MILLISECONDS)
+function alreadyExists(bookmark_url: string) {
+    // If the bookmark already exists in the expendables folder, we don't want
+    // to create it again.
+    return browser.bookmarks.search( {url: bookmark_url})
+        // We'll look for a bookmark matching this URL.
+        .then( bookmark_items => {
+            return bookmark_items[0]
         })
+        .then( found_bookmark => {
+            console.log("Checking found bookmark for expendableness...")
+            // If that bookmark exists and it's also in the expendable folder, we
+            // know it's a duplicate.
+            if (found_bookmark && isExpendable(found_bookmark)) {
+                console.log("Already have expendable bookmark here.")
+                return true
+            }
+        })
+        .catch( () => {
+            // Otherwise, it's not a duplicate and we can continue with expendable
+            // bookmark creation.
+            return false
+        })  
+}
+
+function addExpendableBookmark(bookmark: browser.bookmarks.BookmarkTreeNode) {
+    // We want to avoid adding duplicate expendable bookmarks.
+    if (!expendable_bookmark_set.has(bookmark.id)) {
+        expendable_bookmark_set.add(bookmark.id)
+        
+        console.log(`Added new expendable bookmark ${bookmark.title}.`)
+        
+        browser.notifications.create( {
+            type: "basic",
+            title: "Expendable Bookmark Created!",
+            message: `New expendable bookmark created: ${bookmark.title}`,
+            iconUrl: NOTIFICATION_ICON
+        })
+            .then( notification_id => {
+                // We'll wipe the notification after a timeout.
+                setTimeout(() => 
+                    browser.notifications.clear(notification_id),
+                    NOTIFICATION_TIMEOUT_MILLISECONDS)
+            })
+    } else {
+        console.log("Already had an expendable bookmark here.")
+    }
 }
 
 function removeExpendableBookmark(tab: browser.tabs.Tab) {
     console.log(
         `Looking for expendable bookmark in tab ${tab.title} with url ${tab.url}`)
     
+    // Search for an bookmark with the current tab's URL.
     browser.bookmarks.search( {url: tab.url} )
         .then( bookmark_items => {                        
             return bookmark_items[0]
@@ -103,15 +153,15 @@ function removeExpendableBookmark(tab: browser.tabs.Tab) {
                 console.log(current_bookmark.title, current_bookmark.url)
             }
     
+            // If the bookmark exists and is expendable, we'll remove it.
             if (isExpendable(current_bookmark)) {
-                // expendable_bookmark_set.delete(current_bookmark.id)
                 console.log(`Removing expendable bookmark ${current_bookmark.title}`)
                 browser.bookmarks.remove(current_bookmark.id)
             }
         })
         .catch( error => {
             if (error) {
-                console.log("Couldn't remove expendable bookmark: ${error}")
+                console.log(`Couldn't remove expendable bookmark: ${error}`)
             } else {
                 console.log("Loaded tab is not expendable.")
             }
@@ -132,6 +182,7 @@ function handleOutsideBookmarkCreation(
     id: string,
     bookmark: browser.bookmarks.BookmarkTreeNode) {
     console.log(`Outside bookmark created: ${bookmark.title} at ${bookmark.url}`)
+    // We only care about bookmarks being created in the expendable folder.
     if (bookmark.parentId === FOLDER_ID) {
         console.log("Bookmark was created in expendable folder!")
         addExpendableBookmark(bookmark)
@@ -139,6 +190,7 @@ function handleOutsideBookmarkCreation(
 }
 
 function handleOutsideBookmarkRemoval(id: string, remove_info: any) {
+    // We only care about bookmarks being removed from the expendable folder.
     if (remove_info.parentId === FOLDER_ID) {
         expendable_bookmark_set.delete(remove_info.node.id)        
 
@@ -149,6 +201,7 @@ function handleOutsideBookmarkRemoval(id: string, remove_info: any) {
         iconUrl: NOTIFICATION_ICON
         })
             .then( notification_id => {
+                // We'll wipe the notification after a timeout.
                 setTimeout( () => 
                     browser.notifications.clear(notification_id),
                     NOTIFICATION_TIMEOUT_MILLISECONDS)
@@ -188,7 +241,7 @@ const filter = {
 browser.tabs.onUpdated.addListener(handleTabUpdate, filter)
 
 
-// Listen for creation of new bookmarks in the expendable folder.
+// Listen for creation of new bookmarks.
 browser.bookmarks.onCreated.addListener(handleOutsideBookmarkCreation)
 // Listen for removal of bookmarks.
 browser.bookmarks.onRemoved.addListener(handleOutsideBookmarkRemoval)
